d35f712165ab138b0d6a73c03ebc0656b835fe01
abdessalam-elboukri <abdessalamelboukri00@gmail.com>
2023-04-11 14:55:01 +0000

    add component to view messages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ChatInterfaceComponent} from "./components/chat-interface/chat-interface.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:ChatInterfaceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
